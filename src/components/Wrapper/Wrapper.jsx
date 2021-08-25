094d808a88e6de5ef7a74b4ec579d15c61a3e6ec
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2021-08-25 09:29:03 +0300

    add contacts filtration

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -2,12 +2,19 @@ import { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import ContactForm from '../ContactForm/ContactForm';
 import ContactList from '../ContactList/ContactList';
+import Filter from '../Filter/Filter';
 import Section from '../Section/Section';
 import s from './Wrapper.module.css';
 
 class Wrapper extends Component {
   state = {
-    contacts: [{ id: 'testID', name: 'testName', number: 'testNumber' }],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
   formSubmitHanler = data => {
@@ -22,14 +29,25 @@ class Wrapper extends Component {
     }));
   };
 
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
   render() {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+
     return (
       <div className={s.wrapper}>
         <Section title="Phonebook">
           <ContactForm onSubmit={this.formSubmitHanler} />
         </Section>
         <Section title="Contacts">
-          <ContactList contacts={this.state.contacts} />
+          <Filter value={filter} onChange={this.changeFilter} />
+          <ContactList contacts={filteredContacts} />
         </Section>
       </div>
     );
